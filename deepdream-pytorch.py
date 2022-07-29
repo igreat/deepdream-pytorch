c6ab8139b9e5c14fd9dc4b4a4a1005d92f22c371
igreat <goraguy5@gmail.com>
2022-07-28 19:46:36 -0700

    Add feature extraction

--- deepdream-pytorch.py
+++ deepdream-pytorch.py
@@ -1,10 +1,11 @@
 import torch
-from torchvision.models import inception_v3, Inception_V3_Weights
+from torchvision.models import vgg19_bn, VGG19_BN_Weights
 import utils
 from PIL import Image
 import torch.nn.functional as F
 from torch import optim
 from torchvision.utils import save_image
+from models import VGG19
 
 device = {torch.has_cuda: "cuda", torch.has_mps: "mps"}.get(True, "cpu")
 
@@ -13,29 +14,39 @@ def train(gen_img, loss_model, optimizer):
     pass
 
 
-def main():
-    loss_model = inception_v3(weights=Inception_V3_Weights.DEFAULT).eval().to(device)
-    loss_model.requires_grad_(False)
-
+def deepdream(args=None):
+    features = VGG19(device=device)
     image = Image.open("temp.png")
     image = utils.preprocess(image).to(device)
-    print(torch.argmax(F.softmax(loss_model(image))))
 
-    gen_img = torch.randn(size=(1, 3, 512, 512)).to(device) * 0.5
+    for step in range(200):
+        ...
 
-    optimizer = optim.SGD([gen_img], lr=5e-2)
+
+# generates what the model thinks a particular class is
+def generate_class(args=None):
+    loss_model = vgg19_bn(weights=VGG19_BN_Weights.DEFAULT).eval().to(device)
+    loss_model.requires_grad_(False)
+
+    # image = Image.open("temp.png")
+    # image = utils.preprocess(image).to(device)
+    # print(torch.argmax(F.softmax(loss_model(image))))
+
+    gen_img = torch.randn(size=(1, 3, 512, 512)).to(device) * 0.25 + 0.5
+
+    optimizer = optim.SGD([gen_img], lr=1e-2)
     gen_img.requires_grad_(True)
 
     for step in range(4000):
         optimizer.zero_grad()
 
-        target_loss = -loss_model(gen_img).squeeze(0)[954]
+        target_loss = -loss_model(gen_img).squeeze(0)[282]
         tv_loss = utils.tv_loss(gen_img)
         total_loss = 1e2 * target_loss + 1 * tv_loss
 
         with torch.no_grad():
             if step % 500 == 0:
-                save_image(gen_img.squeeze(0) + 0.5, "temp.png")
+                save_image(gen_img.squeeze(0), "temp.png")
             if step % 100 == 0:
                 print(
                     f"activation: {-target_loss.item()}, tv_loss: {tv_loss.item()} epoch: {step}"
@@ -44,8 +55,8 @@ def main():
         total_loss.backward()
         optimizer.step()
 
-    save_image(gen_img.squeeze(0) + 0.5, "test-img.png")
+    save_image(gen_img.squeeze(0), "test-img.png")
 
 
 if __name__ == "__main__":
-    main()
+    deepdream()
---
+++ models.py
@@ -0,0 +1,53 @@
+# feature extraction
+
+from torchvision.models import vgg19, VGG19_Weights
+import torch.nn as nn
+
+
+class VGG19(nn.Module):
+    def __init__(
+        self,
+        layers=["relu1_1", "relu2_1", "relu3_1", "relu4_1", "relu5_1"],
+        device="cpu",
+    ):
+        super(VGG19, self).__init__()
+        features = vgg19(weights=VGG19_Weights.DEFAULT).features.eval().to(device)
+        features.requires_grad_(False)
+
+        self.slices = []
+        slice_temp = nn.Sequential()
+        pool_cnt, relu_count, conv_count = 1, 1, 1
+        for i in range(len(features)):
+            x = features[i]
+            if isinstance(x, nn.Conv2d):
+                name = f"conv{pool_cnt}_{conv_count}"
+                conv_count += 1
+            elif isinstance(x, nn.ReLU):
+                name = f"relu{pool_cnt}_{relu_count}"
+                relu_count += 1
+            else:
+                name = f"pool{pool_cnt}"
+
+                relu_count = 1
+                conv_count = 1
+                pool_cnt += 1
+
+            slice_temp.add_module(name, x)
+            if name in layers:
+                self.slices += slice_temp
+                print(slice_temp)
+                slice_temp = nn.Sequential()
+                layers.remove(name)
+
+            # making sure it is cut off at the last loss layer to avoid unnecesarry computations
+            if len(layers) == 0:
+                break
+
+    def forward(self, input):
+        activations = []
+        x = input
+        for slice in self.slices:
+            x = slice(x)
+            activations += x
+
+        return activations
